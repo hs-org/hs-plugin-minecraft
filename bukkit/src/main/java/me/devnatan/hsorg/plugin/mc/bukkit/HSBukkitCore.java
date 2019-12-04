85abf583c46ba38d68cf1b607bdceab10cc59a50
DevNatan <itroken99@gmail.com>
2019-12-04 16:57:21 -0300

    Initialize some things from the project.

---
+++ bukkit/src/main/java/me/devnatan/hsorg/plugin/mc/bukkit/HSBukkitCore.java
@@ -0,0 +1,15 @@
+package me.devnatan.hsorg.plugin.mc.bukkit;
+
+import me.devnatan.hsorg.plugin.mc.HSCore;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HSBukkitCore extends JavaPlugin {
+
+    private HSCore backend;
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
---
+++ core/src/main/java/me/devnatan/hsorg/plugin/mc/HSCore.java
@@ -0,0 +1,4 @@
+package me.devnatan.hsorg.plugin.mc;
+
+public class HSCore {
+}
