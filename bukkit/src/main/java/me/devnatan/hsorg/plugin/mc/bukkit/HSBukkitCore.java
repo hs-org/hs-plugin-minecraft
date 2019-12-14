7af257d69cf51f30d4ee278acd852e8fc73da178
DevNatan <itroken99@gmail.com>
2019-12-14 16:46:36 -0300

    Disable the plugin if the connection is not successful.

--- bukkit/src/main/java/me/devnatan/hsorg/plugin/mc/bukkit/HSBukkitCore.java
+++ bukkit/src/main/java/me/devnatan/hsorg/plugin/mc/bukkit/HSBukkitCore.java
@@ -1,7 +1,12 @@
 package me.devnatan.hsorg.plugin.mc.bukkit;
 
 import me.devnatan.hsorg.plugin.mc.HSCore;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class HSBukkitCore extends JavaPlugin {
 
@@ -9,7 +14,26 @@ public class HSBukkitCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        Map<String, String> credentials = new HashMap<>();
+        for (Map.Entry<String, Object> kv : getConfig().getConfigurationSection("api.credentials").getValues(false).entrySet()) {
+            credentials.put(kv.getKey(), kv.getValue().toString());
+        }
 
+        backend = new HSCore(getLogger(), getName(), getConfig().getString("api.endpoint"), credentials);
+        getLogger().info("Starting...");
+
+        try {
+            backend.start().get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            Bukkit.getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        backend.stop();
     }
 
 }
